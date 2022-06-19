c118e37a80f1283d721cd572915d567b6cedfe77
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-20 01:38:09 +0300

    commit tree 34172e5eaf9b039cc78e6f22e51c1cc01f51f922

---
+++ index.js
@@ -0,0 +1,54 @@
+//myEach
+function myEach(collection, callback){
+    Object.values(collection).forEach(callback)
+    return collection;
+}
+
+//myMap
+function myMap(collection, callback) {
+    return Object.values(collection).map(callback);
+}
+     
+//myReduce
+function myReduce(collection, callback, acc) {
+    return Object.values(collection,).reduce(callback, acc = 0);
+}
+
+//myFilter
+function myFilter(collection, callback){
+    return Object.values(collection).filter(callback);
+}
+
+//myFind
+function myFind(collection, callback){
+    return Object.values(collection).find(callback);
+
+}
+
+//mySize
+function mySize(collection){
+    return Object.values (collection).length};
+
+
+//myFirst
+function myFirst (arr, stop=false) {
+    return (stop) ? arr.slice(0, stop) : arr[0];
+  }
+  
+//myLast  
+function myLast(arr, start=false) {
+    return (start) ? arr.slice(arr.length-start, arr.length) : arr[arr.length-1];
+  }
+
+
+//myKeys
+function myKeys (collection){
+    return Object.keys(collection);
+}
+
+
+//myvalues
+function myValues(collection) {
+    return Object.values(collection);
+}
+    
